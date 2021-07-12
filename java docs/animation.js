d7410d9ca7e0ba591152a1626a23b041a5bab642
Ivan-Noriega <81253585+Ivan-Noriega@users.noreply.github.com>
2021-07-12 17:10:07 -0500

    sdfhuosd

---
+++ java docs/animation.js
@@ -0,0 +1,28 @@
+window.sr = ScrollReveal();
+
+sr.reveal("header", {
+    duration: 3000,
+    origin: "top",
+    distance: "-100px"
+});
+sr.reveal(".article-container", {
+    duration: 11000,
+    origin: "top",
+    distance: "-100px"
+});
+sr.reveal(".hero-banner", {
+    duration: 5000,
+    origin: "top",
+    distance: "-100px"
+});
+sr.reveal(".span1", {
+    duration: 7000,
+    origin: "top",
+    distance: "-100px"
+});
+sr.reveal(".span2", {
+    duration: 9000,
+    origin: "top",
+    distance: "-100px"
+});
+    
