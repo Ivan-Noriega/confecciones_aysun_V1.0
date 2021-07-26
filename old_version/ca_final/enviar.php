a1d11a6794d43f0866a95891ce53de5c9f9282ee
Ivan Noriega <noriegaivan625@gmail.com.com>
2021-07-26 11:09:56 -0500

    sexta entrega dia 2

--- old_version/ca_final/enviar.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $destino="Ivan_Noriega1@outlook.com";
-    $correo=$_POST["email"];
-    $mensaje=$_POST["mensaje"];
-    mail($correo, "contacto", $mensaje);
-    header("location:dashboard.php")
-?>
